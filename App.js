04945dc9c1cd2580eaa9a94256baa209fe368e6d
ataraxia7899 <ataraxia7899@gmail.com>
2025-01-21 23:47:50 +0900

    item.key에서 item.id로 수정

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ export default function App() {
                         );
                     }}
                     keyExtractor={(item, index) => {
-                        return item.key;
+                        return item.id;
                     }}
                 />
             </View>
