6dda849d049550d70629a392330cc2d27cdfbc0d
ataraxia7899 <ataraxia7899@gmail.com>
2025-03-10 04:04:14 +0900

    배경색 설정

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import * as SystemUI from 'expo-system-ui';
+
+SystemUI.setBackgroundColorAsync('#24180f');
+// expo-system-ui를 사용해 배경색을 설정
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<StatusBar style="auto" />
+			<StatusBar style="light" />
 			<CategoriesScreen />
 		</View>
 	);
