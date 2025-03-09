3a0682ed2ba833de35d453fb1553e8ad8b5fd494
ataraxia7899 <ataraxia7899@gmail.com>
2025-03-10 00:37:20 +0900

    FlatList로 값을 출력해주는 컴포넌트

---
+++ components/screens/CategoriesScreen.js
@@ -0,0 +1,22 @@
+import { FlatList } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../CategoryGridTile';
+
+function renderCategoryItem(itemData) {
+	return (
+		<CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+	);
+}
+// 위의 함수는 다시 렌더링 혹은 다시 생성할 필요가 없으므로 컴포넌트 함수 밖에서 정의됨
+
+export default function CategoriesScreen() {
+	return (
+		<FlatList
+			data={CATEGORIES}
+			keyExtractor={(item) => item.id}
+			renderItem={renderCategoryItem}
+			numColumns={2} // 열을 강제로 추가
+		/>
+	);
+}
